a90617020e9c44fb70e7e4ba553e432cf34ed7d3
Bruno Rodrigues <bytebruno@gmail.com>
2021-01-31 22:00:18 -0300

    docs: Updated README.md

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import { makeStyles } from '@material-ui/core/styles'
@@ -8,8 +8,6 @@ import CardContent from '@material-ui/core/CardContent'
 
 import Typography from '@material-ui/core/Typography'
 import { getAvatar } from '../utils/avatar-helper'
-
-import { handleGetUsers } from '../actions/users'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -52,56 +50,55 @@ const Leaderboard = ({ users }) => {
   const classes = useStyles()
 
   const orderUsersByScore = () =>
-  Object.values(users).sort((a, b) => {
-    return a.questions.length + Object.keys(a.answers).length >
-      b.questions.length + Object.keys(b.answers).length
-      ? -1
-      : 1
-  })
+    Object.values(users).sort((a, b) => {
+      return a.questions.length + Object.keys(a.answers).length >
+        b.questions.length + Object.keys(b.answers).length
+        ? -1
+        : 1
+    })
 
-  const orderedUsers = orderUsersByScore();
+  const orderedUsers = orderUsersByScore()
 
   return (
     <div>
       {orderedUsers.map((user) => {
-         console.log(user)
         return (
-       
-        <Card key={user.id} className={classes.root} raised>
-          <CardContent className={classes.cardContent}>
-            <div className={classes.nameContainer}>
-              <Avatar
-                src={getAvatar(user.avatarURL)}
-                className={classes.avatar}
-              />
-              <Typography
-                variant='h6'
-                component='h6'
-                className={classes.nameText}
-              >
-                {user.name}
-              </Typography>
-              <Typography
-                variant='h6'
-                component='h6'
-                className={classes.total}
-                color='secondary'
-              >
-                {Object.keys(user.answers).length + user.questions.length}
-              </Typography>
-            </div>
+          <Card key={user.id} className={classes.root} raised>
+            <CardContent className={classes.cardContent}>
+              <div className={classes.nameContainer}>
+                <Avatar
+                  src={getAvatar(user.avatarURL)}
+                  className={classes.avatar}
+                />
+                <Typography
+                  variant='h6'
+                  component='h6'
+                  className={classes.nameText}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  variant='h6'
+                  component='h6'
+                  className={classes.total}
+                  color='secondary'
+                >
+                  {Object.keys(user.answers).length + user.questions.length}
+                </Typography>
+              </div>
 
-            <div className={classes.scoreContainer}>
-              <Typography variant='body1' color='secondary' component='p'>
-                {`Answered questions: ${Object.keys(user.answers).length}`}
-              </Typography>
-              <Typography variant='body1' color='secondary' component='p'>
-                {`Created questions: ${user.questions.length}`}
-              </Typography>
-            </div>
-          </CardContent>
-        </Card>
-      )})}
+              <div className={classes.scoreContainer}>
+                <Typography variant='body1' color='secondary' component='p'>
+                  {`Answered questions: ${Object.keys(user.answers).length}`}
+                </Typography>
+                <Typography variant='body1' color='secondary' component='p'>
+                  {`Created questions: ${user.questions.length}`}
+                </Typography>
+              </div>
+            </CardContent>
+          </Card>
+        )
+      })}
     </div>
   )
 }
